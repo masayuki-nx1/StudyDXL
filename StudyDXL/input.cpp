95ed494e5e0e0b9180f59f7e4f7a37ecbd06deaf
masayuki <2101125304le@cyber-u.ac.jp>
2024-05-31 00:07:35 +0900

    Added image display function

--- StudyDXL/input.cpp
+++ StudyDXL/input.cpp
@@ -18,9 +18,21 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 	int timer = 0;
 
+	//画像の読み込み
+	int imgBG = LoadGraph("images/background.jpg");
+	int imgCat = LoadGraph("images/Player_cat.png");
+
+	int sizeImgCatWidth = 300, sezeImgCatHeight = 300;
+	int catX = 0, catY = 400;
+
+
 	while (1) {
 		//画面をクリアする
 		ClearDrawScreen();
+		//背景の表示
+		DrawGraph(0, 0, imgBG, FALSE);
+
+
 		timer++;
 		//タイマーを表示
 		DrawFormatString(0, 0, WHITE, "%d", timer);
@@ -36,6 +48,13 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		DrawFormatString(400, 0, WHITE, "(%d,%d)", mouseX, mouseY);
 		if (GetMouseInput() & MOUSE_INPUT_LEFT)DrawString(400, 20, "左ボタン", WHITE);
 		if (GetMouseInput() & MOUSE_INPUT_RIGHT)DrawString(400, 40, "右ボタン", WHITE);
+
+		catX = mouseX - sizeImgCatWidth / 2;
+		catY = mouseY - sezeImgCatHeight / 2;
+
+		//catの表示
+		DrawGraph(catX, catY, imgCat, TRUE);
+
 		//裏画面の内容を表画面に反映する
 		ScreenFlip();
 		WaitTimer(33);
